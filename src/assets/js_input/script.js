644e64ea01868c92c7ae196812c83a32cd30e5ae
StariyWah <aleksejivanov03bk.ru@gmail.com>
2024-09-19 00:02:59 +0300

    0 02

--- src/assets/js_input/script.js
+++ src/assets/js_input/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    AOS.init({
+        once: true,
+    });
     let financeSelectOrigin = document.querySelector('[name="finance-select"]');
     if(financeSelectOrigin !== null && financeSelectOrigin !== undefined) {
         let financeSelect = new Choices(financeSelectOrigin, {
@@ -81,6 +84,100 @@ $(document).ready(function(){
                     slidesPerView: 1.38,
                     spaceBetween: 16,
                 },
+            }
+        });
+    }
+    if(document.querySelectorAll('.swiper-tariffs').length > 0) {
+        let tariffsSlider = new Swiper('.swiper-tariffs .swiper', {
+            direction: 'horizontal',
+            spaceBetween: 16,
+            slidesPerView: 3,
+            navigation: {
+                prevEl: '.swiper-tariffs .swiper-button-prev',
+                nextEl: '.swiper-tariffs .swiper-button-next',
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 1.3,
+                    spaceBetween: 10,
+                },
+                701: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1380: {
+                    slidesPerView: 3,
+                    spaceBetween: 16,
+                },
+            },
+        });
+    }
+    if(document.querySelectorAll('.swiper-video-feedback').length > 0) {
+        let feedbackSlider = new Swiper('.swiper-video-feedback .swiper', {
+            direction: 'horizontal',
+            spaceBetween: 16,
+            slidesPerView: 4,
+            navigation: {
+                prevEl: '.swiper-video-feedback .swiper-button-prev',
+                nextEl: '.swiper-video-feedback .swiper-button-next',
+            },
+            breakpoints: {
+                0: {
+                    slidesPerView: 2,
+                    spaceBetween: 12,
+                },
+                701: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1380: {
+                    slidesPerView: 4,
+                    spaceBetween: 16,
+                },
+            },
+        });
+    }
+    const informationButtons = document.querySelectorAll('#information-button');
+    if(informationButtons.length > 0) {
+        informationButtons.forEach(button => {
+            tippy(button, {
+                content: button.querySelector('.information-button__content').innerHTML,
+                allowHTML: true,
+                arrow: false,
+                placement: 'left',
+                offset: [0, -32],
+                zIndex: 10,
+                trigger: 'click',
+                theme: $(button).attr('data-theme'),
+                maxWidth: 360,
+                interactive: true,
+            });
+        });
+    }
+    Fancybox.bind('[data-fancybox]', {
+    });
+    let directionItems = document.querySelectorAll('.directions__item');
+    if(directionItems.length > 0) {
+        let directionClick = $(document).on('click', '.directions__item', function(e) {
+            if (window.innerWidth < 1380) {
+                directionItems.forEach(element => {
+                    if(element === this) {
+                        $(element).toggleClass('_active');
+                    } else {
+                        $(element).removeClass('_active');
+                    }
+                });
+            }
+        });
+        $('.directions__item').hover(function(e) {
+            if (window.innerWidth >= 1380) {
+                directionItems.forEach(element => {
+                    if(element === this) {
+                        $(element).toggleClass('_active');
+                    } else {
+                        $(element).removeClass('_active');
+                    }
+                });
             }
         });
     }
